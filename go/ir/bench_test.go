975e85cf05b8760c0e7b9bc450985d49df94a6d1
Marcin Skalski <skalskimarcin33@gmail.com>
2026-09-17 08:50:53 +0200

    test(ir): benchmark single-block locals
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- go/ir/bench_test.go
+++ go/ir/bench_test.go
@@ -1,11 +1,58 @@
 package ir_test
 
 import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"strings"
 	"testing"
 
 	"golang.org/x/tools/go/packages"
 	"honnef.co/go/tools/go/ir"
 )
+
+func BenchmarkSSASingleBlockLocals(b *testing.B) {
+	for _, size := range []int{64, 256, 1024} {
+		b.Run(fmt.Sprintf("locals=%d", size), func(b *testing.B) {
+			var src strings.Builder
+			src.WriteString("package bench\nfunc f(a int) int {\n\ttotal := 0\n")
+			for i := range size {
+				fmt.Fprintf(&src, "\tx%d := a\n\ttotal += x%d\n", i, i)
+			}
+			src.WriteString("\treturn total\n}\n")
+
+			fset := token.NewFileSet()
+			file, err := parser.ParseFile(fset, "bench.go", src.String(), parser.SkipObjectResolution)
+			if err != nil {
+				b.Fatal(err)
+			}
+			pkg := types.NewPackage("example.com/bench", "bench")
+			info := &types.Info{
+				Types:        make(map[ast.Expr]types.TypeAndValue),
+				Defs:         make(map[*ast.Ident]types.Object),
+				Uses:         make(map[*ast.Ident]types.Object),
+				Implicits:    make(map[ast.Node]types.Object),
+				Scopes:       make(map[ast.Node]*types.Scope),
+				Selections:   make(map[*ast.SelectorExpr]*types.Selection),
+				Instances:    make(map[*ast.Ident]types.Instance),
+				FileVersions: make(map[*ast.File]string),
+			}
+			if err := types.NewChecker(&types.Config{}, fset, pkg, info).Files([]*ast.File{file}); err != nil {
+				b.Fatal(err)
+			}
+
+			b.ReportAllocs()
+			b.ResetTimer()
+			for b.Loop() {
+				prog := ir.NewProgram(fset, 0)
+				irpkg := prog.CreatePackage(pkg, []*ast.File{file}, info, true)
+				irpkg.Build()
+			}
+		})
+	}
+}
 
 func BenchmarkSSA(b *testing.B) {
 	cfg := &packages.Config{
